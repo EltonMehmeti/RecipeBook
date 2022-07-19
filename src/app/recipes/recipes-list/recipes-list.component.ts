960b32b6cedaab651bb0c9f09ee69aff71f58097
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2022-07-19 13:42:30 +0200

    recipe

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model'
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A test Recipe', 'This is simply a test','https://www.simplyrecipes.com/thmb/RheeF949ewwGy7pxQQNt5v63Oi0=/720x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg')
+];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
